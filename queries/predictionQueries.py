7d53ee0cc2b102e88c817d762a8b15b586b5d08e
Palash Shah <35114859+Palashio@users.noreply.github.com>
2020-06-03 15:34:49 -0400

    changed regression_query_ann + callback functions
    was accidentally using softmax activation for the regression ANN. Modified to just a layer with a single node with a normal kernel initializer. Also changed callback function to min for val_loss.

--- queries/predictionQueries.py
+++ queries/predictionQueries.py
@@ -160,12 +160,14 @@ class client:
             monitor=maximizer,
             mode=callback_mode,
             verbose=1,
-            patience=5)
+            patience=8)
+
 
         i = 0
 
-        # get the first 3 layer model
+        #get the first 3 layer model
         model = get_keras_model_reg(data, i)
+        print(len(model.layers))
 
         logger("training initial model...")
         history = model.fit(
@@ -175,7 +177,8 @@ class client:
             validation_data=(
                 X_test,
                 y_test),
-            callbacks=[es])
+                callbacks=[es])
+            
         models.append(history)
         print(currLog)
 
@@ -194,13 +197,14 @@ class client:
                 epochs=epochs,
                 validation_data=(
                     X_test,
-                    y_test))
+                    y_test),
+                    callbacks=[es])
             models.append(history)
             losses.append(models[i].history[maximizer]
                           [len(models[i].history[maximizer]) - 1])
             i += 1
 
-        # calls function to generate plots in plot generation
+        #calls function to generate plots in plot generation
         if generate_plots:
             init_plots, plot_names = generate_regression_plots(
                 models[len(models) - 1], data, y)
@@ -263,7 +267,7 @@ class client:
         # early stopping callback
         es = EarlyStopping(
             monitor=maximizer,
-            mode='min',
+            mode=callback_mode,
             verbose=1,
             patience=5)
 
