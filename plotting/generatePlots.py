e46d0c3efefe251657a4c0e3b852c4b6b9dcdd5a
Sarthak Chauhan <schauhan@es.iitr.ac.in>
2020-06-05 00:00:31 +0530

    Updated generatePlot.py
    Added the plot_corr() function, plot_acc() and plot_loss()
    Deleted the Accuracy and Loss plots from majority of the functions and added them as separate common to all plots
    Deleted recurrence libraries

--- plotting/generatePlots.py
+++ plotting/generatePlots.py
@@ -14,10 +14,8 @@ from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from tensorflow import keras
 from tensorflow.python.keras.layers import Dense, Input
 from keras.callbacks import EarlyStopping
-from matplotlib import pyplot
 from matplotlib import pyplot as PLT
 from data_preprocesser import structured_preprocesser
 from predictionModelCreation import get_keras_model_reg
@@ -27,10 +25,9 @@ from keras.utils import np_utils
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, roc_curve
+import seaborn as sns
 
 # generates all of the plots in clustering
-
-
 def generate_clustering_plots(kmeans, dataPandas, dataset):
     plots = []
     plot_names = []
@@ -51,20 +48,11 @@ def generate_clustering_plots(kmeans, dataPandas, dataset):
     return plots, plot_names
 
 # generates all of the plots for regression
-
-
 def generate_regression_plots(history, data, label):
     plots = []
     plot_names = []
     # generating plots for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-
-    plots.append(plt)
+    plots.append(plot_loss(history))
     plot_names.append('lossvsval_los')
 
     return plots, plot_names
@@ -75,27 +63,11 @@ def generate_classification_plots(history, data, label, model, X_test, y_test):
     plot_names = []
 
     # generating plots for accuracy
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-
-    plots.append(plt)
+    plots.append(plot_acc(history))
     plot_names.append('accuracyvsval_accuracy')
-
     # generating plots for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-
-    plots.append(plt)
+    plots.append(plot_loss(history))
     plot_names.append('lossvsval_los')
-
 
     # dynamic way to return all possible plots in case it expands together
     return_plots = {}
@@ -105,8 +77,6 @@ def generate_classification_plots(history, data, label, model, X_test, y_test):
     return return_plots
 
 # function to return both val and accuracy plots on one pane
-
-
 def generate_classification_together(history, data, model, X_test, y_test):
     plots = []
     plot_names = []
@@ -118,19 +88,38 @@ def generate_classification_together(history, data, model, X_test, y_test):
 
     # the first loss plot on the top
     plt.subplot(2, 1, 1)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-
+    plot_loss(history)
     # the second accuracy plot on the bottom
     plt.subplot(2, 1, 2)
+    plot_acc(history)
+    plt.show()
+
+def plot_acc(history):
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
+	return plt
 
-    plt.show()
+def plot_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    return plt
+
+def plot_corr(data,col):
+    #Here, col is a string array whichindicates the columns between 
+    #which the correlation is required. if left empty it shows the  
+    #correlation heatmap for all the variables.
+    if not col:
+        data = data.loc[:, data.columns.intersection(['a','b'])]
+        corr = data.corr()
+        sns.heatmap(
+           corr, 
+           vmin=-1, vmax=1, center=0,
+           square=True)
