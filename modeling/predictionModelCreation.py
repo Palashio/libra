Add date parsing and fix data leakage Closes #62 Closes #68

modeling/predictionModelCreation.py
@@ -11,8 +11,8 @@ def get_keras_model_reg(dataset, i):
     model = tf.keras.Sequential()
     model.add(
         Dense(
-            dataset.shape[1],
-            input_dim=dataset.shape[1],
+            dataset['train'].shape[1],
+            input_dim=dataset['train'].shape[1],
             kernel_initializer='normal',
             activation='relu'))
     model.add(Dense(64, activation="relu"))