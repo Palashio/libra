b42d8e6ffd7e7bfd6872172bacf239de7cef2135
Palash Shah <35114859+Palashio@users.noreply.github.com>
2020-06-23 22:58:38 -0400

    got rid of excess

--- libra/queries/test.py
+++
@@ -1,6 +0,0 @@
-from libra.queries.prediction_queries import neural_network_query
-from libra.queries.prediction_queries import  client
-
-newClient = client('/Users/palashshah/desktop/housing.csv').neural_network_query("model ocean proximity")
-
-
