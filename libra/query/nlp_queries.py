35fd640ffceb9b8df292cb8b247e85557e68498b
Anas Awadalla <anasa2@cs.washington.edu>
2020-07-03 16:26:50 -0700

    Added a bit more for gpu on tf

--- libra/query/nlp_queries.py
+++ libra/query/nlp_queries.py
@@ -433,7 +433,8 @@ def image_caption_query(self, instruction,
         if tf.test.gpu_device_name():
             print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
         else:
-            raise Exception("Please install GPU version of TF")
+            raise Exception("Please install GPU version of Tensorflow")
+
         device = '/device:GPU:0'
     else:
         device = '/device:CPU:0'
@@ -547,62 +548,64 @@ def image_caption_query(self, instruction,
 
     @tf.function
     def train_step(img_tensor, target):
-        loss = 0
+        with tf.device(device):
+            loss = 0
 
-        # initializing the hidden state for each batch
-        # because the captions are not related from image to image
-        hidden = decoder.reset_state(batch_size=target.shape[0])
+            # initializing the hidden state for each batch
+            # because the captions are not related from image to image
+            hidden = decoder.reset_state(batch_size=target.shape[0])
 
-        dec_input = tf.expand_dims(
-            [tokenizer.word_index['<start>']] * target.shape[0], 1)
+            dec_input = tf.expand_dims(
+                [tokenizer.word_index['<start>']] * target.shape[0], 1)
 
-        with tf.GradientTape() as tape:
-            features = encoder(img_tensor)
+            with tf.GradientTape() as tape:
+                features = encoder(img_tensor)
 
-            for i in range(1, target.shape[1]):
-                # passing the features through the decoder
-                predictions, hidden, _ = decoder(dec_input, features, hidden)
+                for i in range(1, target.shape[1]):
+                    # passing the features through the decoder
+                    predictions, hidden, _ = decoder(dec_input, features, hidden)
 
-                loss += loss_function(target[:, i], predictions)
+                    loss += loss_function(target[:, i], predictions)
 
-                # using teacher forcing
-                dec_input = tf.expand_dims(target[:, i], 1)
+                    # using teacher forcing
+                    dec_input = tf.expand_dims(target[:, i], 1)
 
-        total_loss = (loss / int(target.shape[1]))
+            total_loss = (loss / int(target.shape[1]))
 
-        trainable_variables = encoder.trainable_variables + decoder.trainable_variables
+            trainable_variables = encoder.trainable_variables + decoder.trainable_variables
 
-        gradients = tape.gradient(loss, trainable_variables)
+            gradients = tape.gradient(loss, trainable_variables)
 
-        optimizer.apply_gradients(zip(gradients, trainable_variables))
+            optimizer.apply_gradients(zip(gradients, trainable_variables))
 
-        return loss, total_loss
+            return loss, total_loss
 
     @tf.function
     def val_step(img_tensor, target):
-        loss = 0
+        with tf.device(device):
+            loss = 0
 
-        # initializing the hidden state for each batch
-        # because the captions are not related from image to image
-        hidden = decoder.reset_state(batch_size=target.shape[0])
+            # initializing the hidden state for each batch
+            # because the captions are not related from image to image
+            hidden = decoder.reset_state(batch_size=target.shape[0])
 
-        dec_input = tf.expand_dims(
-            [tokenizer.word_index['<start>']] * target.shape[0], 1)
+            dec_input = tf.expand_dims(
+                [tokenizer.word_index['<start>']] * target.shape[0], 1)
 
-        with tf.GradientTape() as tape:
-            features = encoder(img_tensor)
+            with tf.GradientTape() as tape:
+                features = encoder(img_tensor)
 
-            for i in range(1, target.shape[1]):
-                # passing the features through the decoder
-                predictions, hidden, _ = decoder(dec_input, features, hidden)
+                for i in range(1, target.shape[1]):
+                    # passing the features through the decoder
+                    predictions, hidden, _ = decoder(dec_input, features, hidden)
 
-                loss += loss_function(target[:, i], predictions)
+                    loss += loss_function(target[:, i], predictions)
 
-                # using teacher forcing
-                dec_input = tf.expand_dims(target[:, i], 1)
+                    # using teacher forcing
+                    dec_input = tf.expand_dims(target[:, i], 1)
 
-        total_loss = (loss / int(target.shape[1]))
-        return total_loss
+            total_loss = (loss / int(target.shape[1]))
+            return total_loss
 
     logger("Training model...")
     with tf.device(device):
