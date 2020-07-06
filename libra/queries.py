ade831e0fcc7abc4908c7f35425446e952644fe3
Palash Shah <35114859+Palashio@users.noreply.github.com>
2020-07-06 11:40:52 -0400

    added ssl verification

--- libra/queries.py
+++ libra/queries.py
@@ -88,6 +88,12 @@ class client:
         clearLog()
 
     def required_installations(self):
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        except AttributeError:
+            pass
+        else:
+            ssl._create_default_https_context = _create_unverified_https_context
         nltk.download('punkt')
         nltk.download('averaged_perceptron_tagger')
 
