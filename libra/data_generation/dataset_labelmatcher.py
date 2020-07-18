a19c2274caf95565a33ea9858cfd7822340f0101
hack_boi <vabhilash2000@gmail.com>
2020-07-18 13:28:28 +0530

    Updated To Comprehensions-dataset_labelmatcher.py

--- libra/data_generation/dataset_labelmatcher.py
+++ libra/data_generation/dataset_labelmatcher.py
@@ -12,23 +12,17 @@ def produceMask(instruction_label):
     ascii_list = list(string.ascii_lowercase)
     res = {i: instruction_label.count(i) for i in set(instruction_label)}
 
-    mask = [0] * len(ascii_list)
 
     # stores the masks in an object with the base mask and returns
-    for x in range(len(ascii_list)):
-        if ascii_list[x] in res.keys():
-            mask[x] = res[str(ascii_list[x])]
+    mask = [res[str(ascii_list[x])] for x in range(len(ascii_list)) if ascii_list[x] in res.keys()]
 
     return mask
 
 
 def get_similar_column(instruction, dataset):
     # instruction = produceMask(instruction_label)
-    distances = []
+    distances = [Levenshtein.distance(instruction, element) for element in dataset.columns]
 
-    for element in dataset.columns:
-        value = Levenshtein.distance(instruction, element)
-        distances.append(value)
 
     val, idx = min((val, idx) for (idx, val) in enumerate(distances))
 
@@ -39,10 +33,7 @@ def get_similar_column(instruction, dataset):
 # exact exame to get_similar_column(). Adapted to allow for small changes
 # for model similarity identification
 def get_similar_model(model_requested, model_keys):
-    distances = []
-
-    for element in model_keys:
-        distances.append(Levenshtein.distance(model_requested, element))
+    distances = [Levenshtein.distance(model_requested, element) for element in model_keys]
 
     val, idx = min((val, idx) for (idx, val) in enumerate(distances))
 
