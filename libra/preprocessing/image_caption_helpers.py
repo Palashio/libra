2c4fa9c5c2ce175ab29b68f97f0788dd588e7139
Anas Awadalla <anasa2@cs.washington.edu>
2020-07-20 09:48:43 -0700

    Added comments on image_caption_helpers.py

--- libra/preprocessing/image_caption_helpers.py
+++ libra/preprocessing/image_caption_helpers.py
@@ -38,6 +38,11 @@ def map_func(img_name, cap):
     return img_tensor, cap
 
 
+"""
+Creates BahdanauAttention model
+"""
+
+
 class BahdanauAttention(tf.keras.Model):
     def __init__(self, units):
         super(BahdanauAttention, self).__init__()
@@ -46,24 +51,18 @@ class BahdanauAttention(tf.keras.Model):
         self.V = tf.keras.layers.Dense(1)
 
     def call(self, features, hidden):
-        # features(CNN_encoder output) shape == (batch_size, 64, embedding_dim)
-
-        # hidden shape == (batch_size, hidden_size)
-        # hidden_with_time_axis shape == (batch_size, 1, hidden_size)
         hidden_with_time_axis = tf.expand_dims(hidden, 1)
-
-        # score shape == (batch_size, 64, hidden_size)
         score = tf.nn.tanh(self.W1(features) + self.W2(hidden_with_time_axis))
-
-        # attention_weights shape == (batch_size, 64, 1)
-        # you get 1 at the last axis because you are applying score to self.V
         attention_weights = tf.nn.softmax(self.V(score), axis=1)
-
-        # context_vector shape after sum == (batch_size, hidden_size)
         context_vector = attention_weights * features
         context_vector = tf.reduce_sum(context_vector, axis=1)
 
         return context_vector, attention_weights
+
+
+"""
+Creates CNN Encoder
+"""
 
 
 class CNN_Encoder(tf.keras.Model):
@@ -78,6 +77,11 @@ class CNN_Encoder(tf.keras.Model):
         x = self.fc(x)
         x = tf.nn.relu(x)
         return x
+
+
+"""
+Creates RNN decoder
+"""
 
 
 class RNN_Decoder(tf.keras.Model):
@@ -123,6 +127,11 @@ class RNN_Decoder(tf.keras.Model):
 
     def reset_state(self, batch_size):
         return tf.zeros((batch_size, self.units))
+
+
+"""
+Generates captions from image
+"""
 
 
 def generate_caption_helper(image,
