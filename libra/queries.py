9d314c2bf897a2af88c362cf606c1b780f325b18
Palash Shah <35114859+Palashio@users.noreply.github.com>
2020-07-22 22:40:45 -0400

    added num clusters change

--- libra/queries.py
+++ libra/queries.py
@@ -346,6 +346,7 @@ class client:
                                 scatters=[],
                                 generate_plots=True,
                                 drop=None,
+                                clusters=None,
                                 base_clusters=2,
                                 verbose=0,
                                 n_init=10,
@@ -374,6 +375,7 @@ class client:
         self.models['k_means_clustering'] = k_means_clustering(
             dataset=self.dataset,
             scatters=scatters,
+            clusters=clusters,
             preprocess=preprocess,
             generate_plots=generate_plots,
             drop=drop,
