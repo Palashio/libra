28639b85cc63e107727a26122c9263a6e2e21cda
Vagif <vagal2003@gmail.com>
2020-07-25 22:54:33 +0100

    Clean version of content based recommender

--- mytest.py
+++
@@ -1,6 +0,0 @@
-from libra import client
-import pandas as pd
-c = client('/Users/Vagif/Downloads/disney_plus_shows.csv')
-c.content_recommender_query(feature_names=['genre','plot','director'],indexer='title')
-qw = c.recommend('Toy Story')
-print(qw)
