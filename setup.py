fd691f7841660d77131185cfd998f2a5f0a4a26d
Palash Shah <35114859+Palashio@users.noreply.github.com>
2020-07-25 19:22:43 -0400

    removal from torch

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setuptools.setup(
                       'keras-tuner',
                       'spacy',
                       'jellyfish',
-                      'torch',
                       'autocorrect',
                       'pillow',
                       'prince',
