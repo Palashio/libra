977c7959146da433324ad9fd28818ac7d363c85d
abx393 <abandari99@gmail.com>
2020-07-30 19:56:59 -0700

    removing later CNN layers

--- libra/query/feedforward_nn.py
+++ libra/query/feedforward_nn.py
@@ -654,7 +654,7 @@ def convolutional(instruction=None,
         model.add(Conv2D(256, (3, 3), activation='relu', padding='SAME'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-        model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
+        '''model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
@@ -664,7 +664,7 @@ def convolutional(instruction=None,
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))'''
 
         model.add(Flatten())
         model.add(Dense(4096, activation='relu'))
