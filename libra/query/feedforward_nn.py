8894450b441f86b2743c4dfd575ed6d2deb6c2a0
abx393 <abandari99@gmail.com>
2020-07-30 20:04:14 -0700

    removing more layers

--- libra/query/feedforward_nn.py
+++ libra/query/feedforward_nn.py
@@ -644,6 +644,7 @@ def convolutional(instruction=None,
         model.add(Conv2D(64, (3, 3), activation='relu', padding='SAME'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
+        '''
         model.add(Conv2D(128, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(128, (3, 3), activation='relu', padding='SAME'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
@@ -654,7 +655,7 @@ def convolutional(instruction=None,
         model.add(Conv2D(256, (3, 3), activation='relu', padding='SAME'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-        '''model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
+        model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(512, (3, 3), activation='relu', padding='SAME'))
