36f25ceb221bbfabf10982528902d86e8f1f213f
abx393 <abandari99@gmail.com>
2020-07-30 21:01:36 -0700

    different arch

--- libra/query/feedforward_nn.py
+++ libra/query/feedforward_nn.py
@@ -644,13 +644,14 @@ def convolutional(instruction=None,
         model.add(Conv2D(64, (3, 3), activation='relu', padding='SAME'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-        '''
         model.add(Conv2D(128, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(128, (3, 3), activation='relu', padding='SAME'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
         model.add(Conv2D(256, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(256, (3, 3), activation='relu', padding='SAME'))
+
+        '''
         model.add(Conv2D(256, (3, 3), activation='relu', padding='SAME'))
         model.add(Conv2D(256, (3, 3), activation='relu', padding='SAME'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
@@ -668,8 +669,7 @@ def convolutional(instruction=None,
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))'''
 
         model.add(Flatten())
-        model.add(Dense(4096, activation='relu'))
-        model.add(Dense(4096, activation='relu'))
+        model.add(Dense(1024, activation='relu'))
         model.add(Dense(num_classes, activation=output_layer_activation))
 
     model.compile(
